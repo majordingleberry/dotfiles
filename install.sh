3634078c7c89580b70e36cfd066bd6dcc7abf73b
majordingleberry <major.dingleberry@outlook.com>
2024-07-31 08:48:01 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+
+
+sudo apt update && sudo apt upgrade -you
+##next up - utilities
+
+# if its really bare
+sudo apt install openSSH-server
+
+# general progs
+sudo apt install unzip git tree stow zoxide zsh neovim
+
+# oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /bin
+
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# my dotfiles
+git clone git@gitea.lab.nyga.us:andreas/dotfiles.git ~/dotfiles
+
+
+# apply them
+cd ~/dotfiles
+stow zsh
+stow oh-my-posh
+stow nvim
+stow an
